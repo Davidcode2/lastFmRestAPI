ccad6ede1c3ce136b02726ae1ad025932b4734b7
Jakob Lingel <jlingel68@gmail.com>
2022-09-02 09:06:43 +0200

    Display artists, no image rendering

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,11 +1,61 @@
 import axios from 'axios';
+import './style.css'
 
-let searchButton = document.querySelector(".searchButton");
-if (searchButton) {
-  searchButton.addEventListener('click', () => {
-    console.log("worked");
+class Search {
+
+  output = document.querySelector(".output");
+  searchButton = document.querySelector(".searchButton");
+
+  callback() {
+    let searchArgument = document.querySelector("input")!.value;
+    console.log(searchArgument);
+    let apiString = `/api/get?artist=${searchArgument}`;
+    console.log(apiString);
     axios
-    .get('/api/search-artist')
-    .then(res => console.log(res));
-  });
+      .get(apiString)
+      .then(res => {
+        let artists = this.extractArtists(res);
+        this.clearOutput();
+        this.renderArtists(artists);
+        console.log(artists);
+      });
+  }
+
+  clearOutput() {
+    this.output.innerHTML = "";
+  }
+
+  createButton(callback) {
+    if (this.searchButton) {
+      this.searchButton.addEventListener('click', callback.bind(this));
+    }
+  }
+
+  extractArtists(res) {
+    return res.data.artist;
+  }
+
+  renderArtists(artists) {
+    for (let artist of artists) {
+      this.createCardForArtist(artist);
+    }
+  }
+
+  createCardForArtist(artist) {
+    let card = document.createElement("div");
+    card.classList.add("card");
+    for (const [key, value] of Object.entries(artist)) {
+      let p = document.createElement("p");
+      p.innerHTML += `${key}: ${value}`;
+      card.appendChild(p);
+    }
+    this.output.appendChild(card);
+  }
+
+  start() {
+    this.createButton(this.callback);
+  }
 }
+
+let app = new Search();
+app.start();
