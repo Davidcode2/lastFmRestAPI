5c5f7f5216dc4701d264021d132121b597de23de
Jakob Lingel <jlingel68@gmail.com>
2022-09-03 00:41:27 +0200

    Move artist retrieval to appropriate file

--- frontend/src/artists.ts
+++ frontend/src/artists.ts
@@ -1,3 +1,68 @@
-class artists {
+import axios from 'axios';
 
+export class Artists {
+
+  private output = document.querySelector(".output");
+  private searchButton = document.querySelector(".searchButton");
+
+  start() {
+    this.createButton(this.getArtists);
+  }
+
+  createButton(callback) {
+    if (this.searchButton) {
+      this.searchButton.addEventListener('click', callback.bind(this));
+      this.confirmOnEnter();
+    }
+  }
+
+  getArtists() {
+    let searchArgument = document.querySelector("input")!.value;
+    console.log(searchArgument);
+    let apiString = `/api/get?artist=${searchArgument}`;
+    console.log(apiString);
+    axios
+      .get(apiString)
+      .then(res => {
+        let artists = this.extractArtists(res);
+        this.clearOutput();
+        this.renderArtists(artists);
+        console.log(artists);
+      });
+  }
+
+  confirmOnEnter() {
+    let input = document.querySelector("#artist-name");
+    input.addEventListener("keyup", event => {
+      let keyboardEvent = <KeyboardEvent>event;
+      if (keyboardEvent.key === "Enter") {
+        this.getArtists();
+      }
+    });
+  }
+
+  clearOutput() {
+    this.output.innerHTML = "";
+  }
+
+  extractArtists(res) {
+    return res.data.artist;
+  }
+
+  renderArtists(artists) {
+    for (let artist of artists) {
+      this.createCardForArtist(artist);
+    }
+  }
+
+  createCardForArtist(artist) {
+    let card = document.createElement("div");
+    card.classList.add("card");
+    for (const [key, value] of Object.entries(artist)) {
+      let p = document.createElement("p");
+      p.innerHTML += `${key}: ${value}`;
+      card.appendChild(p);
+    }
+    this.output.appendChild(card);
+  }
 }
--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,68 +1,15 @@
-import axios from 'axios';
 import { Csv } from './csv';
+import { Artists } from './artists';
 import './style.css'
 
 class App {
 
-  private output = document.querySelector(".output");
-  private searchButton = document.querySelector(".searchButton");
-
-  callback() {
-    let searchArgument = document.querySelector("input")!.value;
-    console.log(searchArgument);
-    let apiString = `/api/get?artist=${searchArgument}`;
-    console.log(apiString);
-    axios
-      .get(apiString)
-      .then(res => {
-        let artists = this.extractArtists(res);
-        this.clearOutput();
-        this.renderArtists(artists);
-        console.log(artists);
-      });
-  }
-
-  onSubmit() {
-    let form = document.querySelector("form");
-    form.onSubmit = function() {
-      console.log("submitted");
-    }
-  }
-
-  clearOutput() {
-    this.output.innerHTML = "";
-  }
-
-  createButton(callback) {
-    if (this.searchButton) {
-      this.searchButton.addEventListener('click', callback.bind(this));
-    }
-  }
-
-  extractArtists(res) {
-    return res.data.artist;
-  }
-
-  renderArtists(artists) {
-    for (let artist of artists) {
-      this.createCardForArtist(artist);
-    }
-  }
-
-  createCardForArtist(artist) {
-    let card = document.createElement("div");
-    card.classList.add("card");
-    for (const [key, value] of Object.entries(artist)) {
-      let p = document.createElement("p");
-      p.innerHTML += `${key}: ${value}`;
-      card.appendChild(p);
-    }
-    this.output.appendChild(card);
-  }
+  private artists = new Artists();
 
   start() {
-    this.createButton(this.callback);
+    this.artists.start();
   }
+
 }
 
 let app = new App();
